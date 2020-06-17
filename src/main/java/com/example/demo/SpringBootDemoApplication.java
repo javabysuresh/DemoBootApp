b3e5639b7d1cee8a7d3a3267513c31961b2fc3f3
javabysuresh <sureshchary66@gmail.com>
2020-06-17 10:54:31 +0530

    msg added

--- src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -9,6 +9,7 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 		System.out.println("Hello SURESH.....RAmesh");
+		System.out.println("HELLO WORLD")
 	}
 
 }
